1f96d94d87268648d82162ece625b64f2591f5e3
aliyan jabbar <jabbaraliyan805@gmail.com>
2024-09-15 00:56:34 +0500

    milestone -5

--- MileStone-3,4&5/script.js
+++ MileStone-3,4&5/script.js
@@ -1,5 +1,44 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var _a;
-(_a = document.querySelector("#genRes")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function () {
+var _this = this;
+//adding event listener to Generate Resume Button
+(_a = document.querySelector("#genRes")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", function (e) {
+    e.preventDefault; //Prevent reload
     // checking if resume already exists
     var resume = document.getElementById("generatedResume");
     if (resume) {
@@ -37,7 +76,45 @@ var _a;
         var newResume = document.createElement("div");
         newResume.innerHTML = generateResumeHTML();
         newResume.id = "generatedResume";
+        // appending generated resume in body of our document
         document.body.appendChild(newResume);
+        // FOR SHAREABLE LINK
+        //getting user's name
+        var username_1 = document.getElementById("name")
+            .value;
+        // Getting elements in shareable Link div
+        var shareableLink = document.getElementById("ResumeLink");
+        var DownloadBtn = document.getElementById("DownloadBtn");
+        //Download button functionality
+        DownloadBtn.addEventListener("click", function () {
+            window.print();
+        });
+        // copy shareable link functionality
+        shareableLink.addEventListener('click', function () { return __awaiter(_this, void 0, void 0, function () {
+            var link, err_1;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        link = "".concat(window.location.origin, "?").concat(encodeURIComponent(username_1), "_cv.html");
+                        _a.label = 1;
+                    case 1:
+                        _a.trys.push([1, 3, , 4]);
+                        //using clipboard API to copy shareable link
+                        return [4 /*yield*/, navigator.clipboard.writeText(link)];
+                    case 2:
+                        //using clipboard API to copy shareable link
+                        _a.sent();
+                        alert("shareable Link copied to clipboard!");
+                        return [3 /*break*/, 4];
+                    case 3:
+                        err_1 = _a.sent();
+                        console.error("Failed To Copy URL: ", err_1);
+                        alert("Failed To Copy Resume Link!");
+                        return [3 /*break*/, 4];
+                    case 4: return [2 /*return*/];
+                }
+            });
+        }); });
     }
 });
 //making a function to generate resume
@@ -47,34 +124,29 @@ function generateResumeHTML() {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         image: document.getElementById("userImage"),
-        phoneNumber: document.getElementById("number")
-            .value,
+        phoneNumber: document.getElementById("number").value,
         about: document.getElementById("about").value,
     };
     var education = {
-        institute: document.getElementById("Institute")
-            .value,
+        institute: document.getElementById("Institute").value,
         degree: document.getElementById("Degree").value,
-        startYear: document.getElementById("startYear")
-            .value,
+        startYear: document.getElementById("startYear").value,
         endYear: document.getElementById("endYear").value,
     };
     var workExperience = {
         company: document.getElementById("company").value,
-        position: document.getElementById("position")
+        position: document.getElementById("position").value,
+        startYear: document.getElementById("startYearComp")
             .value,
-        startYear: document.getElementById("startYearComp").value,
-        endYear: document.getElementById("endYearComp")
-            .value,
+        endYear: document.getElementById("endYearComp").value,
     };
     //getting skills from user
-    var skills = document.getElementById("Skills")
-        .value;
+    var skills = document.getElementById("Skills").value;
     // creating image url for user provided image
     var imageURL = "";
     if (personalInfo.image.files) {
         imageURL = URL.createObjectURL(personalInfo.image.files[0]); //now we can use it in src of image attribute
     }
     // Generating resume HTML to append
-    return "\n      <!-- Left Side -->\n      <div contenteditable=\"true\" class=\"leftSide\">\n        <!-- personal Information -->\n        <div class=\"personalInfo\">\n        <!-- Image in personal Info -->\n        <div class=\"image\">\n        <img src=\"".concat(imageURL, "\" alt=\"Resume Image\">\n        </div>\n          <!-- Name in personal info -->\n          <div class=\"name\">\n            <h1>").concat(personalInfo.name, "</h1>\n          </div>\n          <!-- Contact details in personal info -->\n          <div class=\"contactDetails\">\n            <p>").concat(personalInfo.phoneNumber, "</p>\n            <p>").concat(personalInfo.email, "<p>\n  \n          </div>\n        </div>\n        \n        <!-- Skills -->\n        <div class=\"skills\">\n        My Skills: ").concat(skills, "\n        </div>\n      </div>\n  \n      <!-- Right Side -->\n      <div contenteditable=\"true\" class=\"rightSide\">\n        <div class=\"about\">\n          <h2 id=\"aboutHead\">About</h2>\n          <p>\n          ").concat(personalInfo.about, "\n            </p>\n        </div>\n        <!-- Education -->\n        <div class=\"edu\">\n          <h2 id=\"edu\">Education</h2>\n    \n          <p><strong>").concat(education.degree, "</strong> from <strong>").concat(education.institute, "</strong></p>\n          <p>").concat(education.startYear, "-").concat(education.endYear, "</p>\n  \n        </div>\n        <!-- Work -->\n        <div class=\"work\">\n          <h2 id=\"work\">Work Experience</h2>\n          <p> ").concat(workExperience.company, "\n            <br>\n            ").concat(workExperience.position, "<br>\n             (").concat(workExperience.startYear, " - ").concat(workExperience.endYear, ") <br>\n            <br>\n            </p>\n        </div>\n      </div>\n        ");
+    return "\n      <!-- Left Side -->\n      <div class=\"leftSide\">\n        <!-- personal Information -->\n        <div class=\"personalInfo\">\n        <!-- Image in personal Info -->\n        <div class=\"image\">\n        <img src=\"".concat(imageURL, "\" alt=\"Resume Image\">\n        </div>\n          <!-- Name in personal info -->\n          <div contenteditable=\"true\" class=\"name\">\n            <h1>").concat(personalInfo.name, "</h1>\n          </div>\n          <!-- Contact details in personal info -->\n          <div contenteditable=\"true\"  class=\"contactDetails\">\n            <p>").concat(personalInfo.phoneNumber, "</p>\n            <p>").concat(personalInfo.email, "<p>\n  \n          </div>\n        </div>\n        \n        <!-- Skills -->\n        <div contenteditable=\"true\" class=\"skills\">\n        My Skills: ").concat(skills, "\n        </div>\n      </div>\n  \n      <!-- Right Side -->\n      <div id= \"rightSide\" class=\"rightSide\">\n        <div contenteditable=\"true\"  class=\"about\">\n          <h2 id=\"aboutHead\">About</h2>\n          <p>\n          ").concat(personalInfo.about, "\n            </p>\n        </div>\n        <!-- Education -->\n        <div contenteditable=\"true\" class=\"edu\">\n          <h2 id=\"edu\">Education</h2>\n    \n          <p><strong>").concat(education.degree, "</strong> from <strong>").concat(education.institute, "</strong></p>\n          <p>").concat(education.startYear, "-").concat(education.endYear, "</p>\n  \n        </div>\n        <!-- Work -->\n        <div contenteditable=\"true\" class=\"work\">\n          <h2 id=\"work\">Work Experience</h2>\n          <p> ").concat(workExperience.company, "\n            <br>\n            ").concat(workExperience.position, "<br>\n             (").concat(workExperience.startYear, " - ").concat(workExperience.endYear, ") <br>\n            <br>\n            </p>\n        </div>\n        <!-- shareable link and download option section -->\n    <div id=\"shareableLink\">\n    <button id=\"ResumeLink\">Copy Resume Link</button>\n      <br />\n      <button id=\"DownloadBtn\">Download As PDF</button>\n    </div>\n\n      </div>\n            ");
 }
--- MileStone-3,4&5/script.ts
+++ MileStone-3,4&5/script.ts
@@ -1,4 +1,6 @@
-document.querySelector("#genRes")?.addEventListener("click", () => {
+//adding event listener to Generate Resume Button
+document.querySelector("#genRes")?.addEventListener("click", (e: Event) => {
+  e.preventDefault; //Prevent reload
 
   // checking if resume already exists
   let resume = document.getElementById("generatedResume");
@@ -7,7 +9,7 @@ document.querySelector("#genRes")?.addEventListener("click", () => {
     alert("Resume Updated Successfully!");
     return;
   }
-  
+
   // Check if all required fields are filled
   const requiredFields = [
     "name",
@@ -37,15 +39,38 @@ document.querySelector("#genRes")?.addEventListener("click", () => {
     alert("Resume Has Been Generated Successfully! Scroll Down To View");
 
     //creating new div for generated-resume
-
     const newResume = document.createElement("div");
     newResume.innerHTML = generateResumeHTML();
     newResume.id = "generatedResume";
-
+    // appending generated resume in body of our document
     document.body.appendChild(newResume);
+
+    // FOR SHAREABLE LINK
+    //getting user's name
+    const username = (document.getElementById("name") as HTMLInputElement)
+      .value;
+    // Getting elements in shareable Link div
+    const shareableLink = document.getElementById("ResumeLink") as HTMLButtonElement;
+    const DownloadBtn = document.getElementById("DownloadBtn") as HTMLButtonElement;
+    //Download button functionality
+    DownloadBtn.addEventListener("click", () => {
+      window.print();
+    });
+    // copy shareable link functionality
+    shareableLink.addEventListener('click',async ()=>{
+      const link = `${window.location.origin}?${encodeURIComponent(username)}_cv.html`
+
+      try{
+        //using clipboard API to copy shareable link
+        await navigator.clipboard.writeText(link)
+        alert("shareable Link copied to clipboard!")
+      }catch(err){
+        console.error("Failed To Copy URL: ", err)
+        alert("Failed To Copy Resume Link!")
+      }
+    })
   }
 });
-
 
 //making a function to generate resume
 function generateResumeHTML() {
@@ -54,43 +79,37 @@ function generateResumeHTML() {
     name: (document.getElementById("name") as HTMLInputElement).value,
     email: (document.getElementById("email") as HTMLInputElement).value,
     image: document.getElementById("userImage") as HTMLInputElement,
-    phoneNumber: (document.getElementById("number") as HTMLInputElement)
-      .value,
+    phoneNumber: (document.getElementById("number") as HTMLInputElement).value,
     about: (document.getElementById("about") as HTMLInputElement).value,
   };
 
   const education = {
-    institute: (document.getElementById("Institute") as HTMLInputElement)
-      .value,
+    institute: (document.getElementById("Institute") as HTMLInputElement).value,
     degree: (document.getElementById("Degree") as HTMLInputElement).value,
-    startYear: (document.getElementById("startYear") as HTMLInputElement)
-      .value,
+    startYear: (document.getElementById("startYear") as HTMLInputElement).value,
     endYear: (document.getElementById("endYear") as HTMLInputElement).value,
   };
 
   const workExperience = {
     company: (document.getElementById("company") as HTMLInputElement).value,
-    position: (document.getElementById("position") as HTMLInputElement)
+    position: (document.getElementById("position") as HTMLInputElement).value,
+    startYear: (document.getElementById("startYearComp") as HTMLInputElement)
       .value,
-    startYear: (
-      document.getElementById("startYearComp") as HTMLInputElement
-    ).value,
-    endYear: (document.getElementById("endYearComp") as HTMLInputElement)
-      .value,
+    endYear: (document.getElementById("endYearComp") as HTMLInputElement).value,
   };
   //getting skills from user
-  const skills = (document.getElementById("Skills") as HTMLInputElement)
-    .value;
+  const skills = (document.getElementById("Skills") as HTMLInputElement).value;
 
   // creating image url for user provided image
   let imageURL = "";
   if (personalInfo.image.files) {
     imageURL = URL.createObjectURL(personalInfo.image.files[0]); //now we can use it in src of image attribute
   }
+
   // Generating resume HTML to append
   return `
       <!-- Left Side -->
-      <div contenteditable="true" class="leftSide">
+      <div class="leftSide">
         <!-- personal Information -->
         <div class="personalInfo">
         <!-- Image in personal Info -->
@@ -98,11 +117,11 @@ function generateResumeHTML() {
         <img src="${imageURL}" alt="Resume Image">
         </div>
           <!-- Name in personal info -->
-          <div class="name">
+          <div contenteditable="true" class="name">
             <h1>${personalInfo.name}</h1>
           </div>
           <!-- Contact details in personal info -->
-          <div class="contactDetails">
+          <div contenteditable="true"  class="contactDetails">
             <p>${personalInfo.phoneNumber}</p>
             <p>${personalInfo.email}<p>
   
@@ -110,21 +129,21 @@ function generateResumeHTML() {
         </div>
         
         <!-- Skills -->
-        <div class="skills">
+        <div contenteditable="true" class="skills">
         My Skills: ${skills}
         </div>
       </div>
   
       <!-- Right Side -->
-      <div contenteditable="true" class="rightSide">
-        <div class="about">
+      <div id= "rightSide" class="rightSide">
+        <div contenteditable="true"  class="about">
           <h2 id="aboutHead">About</h2>
           <p>
           ${personalInfo.about}
             </p>
         </div>
         <!-- Education -->
-        <div class="edu">
+        <div contenteditable="true" class="edu">
           <h2 id="edu">Education</h2>
     
           <p><strong>${education.degree}</strong> from <strong>${education.institute}</strong></p>
@@ -132,7 +151,7 @@ function generateResumeHTML() {
   
         </div>
         <!-- Work -->
-        <div class="work">
+        <div contenteditable="true" class="work">
           <h2 id="work">Work Experience</h2>
           <p> ${workExperience.company}
             <br>
@@ -141,6 +160,13 @@ function generateResumeHTML() {
             <br>
             </p>
         </div>
+        <!-- shareable link and download option section -->
+    <div id="shareableLink">
+    <button id="ResumeLink">Copy Resume Link</button>
+      <br />
+      <button id="DownloadBtn">Download As PDF</button>
+    </div>
+
       </div>
-        `;
+            `;
 }
