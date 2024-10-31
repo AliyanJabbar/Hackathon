245d51caf6b5900dc8823d85c3fcb29ae886e6a6
aliyan jabbar <jabbaraliyan805@gmail.com>
2024-10-31 07:28:53 +0500

    changing in MileStone-1&2

--- MileStone-1&2/script.js
+++ MileStone-1&2/script.js
@@ -12,19 +12,19 @@ var expContent = document.getElementById("exp-content");
 if (!eduContent) {
     eduContent = document.createElement("div");
     eduContent.id = "edu-content";
-    eduContent.innerHTML = "\n    <h3 class=\"newHead\">My Education:</h3>\n    <ul>\n    <li><strong>Completed</strong> SSC Part(1&2).</br> 2022-2024</li>\n    <li><strong>Studying</strong> at (AGSC)Adamjee Govt Science College Karachi. </br> 2024-Onwards</li>\n    </ul>";
+    eduContent.innerHTML = "\n    <h3 class=\"newHead\">My Education</h3>\n    <ul>\n    <li><strong>Completed</strong> SSC Part(1&2).</br> 2022-2024</li>\n    <li><strong>Studying</strong> at (AGSC)Adamjee Govt Science College Karachi. </br> 2024-Onwards</li>\n    </ul>\n";
 }
 //inner HTML for skills
 if (!skillsContent) {
     skillsContent = document.createElement("div");
     skillsContent.id = "skills-content";
-    skillsContent.innerHTML = "\n        <h3 class=\"newHead\">My Skills:</h3>\n        <ul>\n          <li>HTML</li>\n          <li>CSS</li>\n          <li>JavaScript</li>\n          <li>TypeScript</li>\n          <li>Next.JS</li>\n          <li>React.JS</li>\n          <li>Tailwind CSS</li>\n          </ul>\n          ";
+    skillsContent.innerHTML = "\n        <h3 class=\"newHead\">My Skills</h3>\n        \n    <div id= \"allSkills\">\n        <div id=\"html\">\n        <div class=\"htmlSkill\">\n          <span> HTML <i class=\"fab fa-html5\"></i> </span>\n          <span class=\"text-md font-bold text-rose-900\"> 90% </span>\n        </div>\n        <div class=\"htmlSkillBar\">\n          <div class=\"htmlSkillBarLength\"></div>\n        </div>\n      </div>\n\n        <div id=\"css\">\n        <div class=\"cssSkill\">\n          <span> CSS <i class=\"fab fa-css3\"></i> </span>\n          <span> 75% </span>\n        </div>\n        <div class=\"cssSkillBar\">\n          <div class=\"cssSkillBarLength\"></div>\n        </div>\n      </div>\n\n        <div id=\"js\">\n        <div class=\"jsSkill\">\n          <span> JavaScript <i class=\"fab fa-js\"></i> </span>\n          <span> 80% </span>\n        </div>\n        <div class=\"jsSkillBar\">\n          <div class=\"jsSkillBarLength\"></div>\n        </div>\n      </div>\n\n        <div id=\"ts\">\n        <div class=\"tsSkill\">\n          <span> TypeScript <i class=\"fab fa-js\"></i> </span>\n          <span> 90% </span>\n        </div>\n        <div class=\"tsSkillBar\">\n          <div class=\"tsSkillBarLength\"></div>\n        </div>\n      </div>\n\n        <div id=\"nextjS\">\n        <div class=\"nextjsSkill\">\n          <span> Next.JS <img src=\"https://cdn.worldvectorlogo.com/logos/next-js.svg\" alt=\"Next.js Logo\" style=\"width: 20px; height: 20px;\"> </span>\n          <span> 80% </span>\n        </div>\n        <div class=\"nextjsSkillBar\">\n          <div class=\"nextjsSkillBarLength\"></div>\n        </div>\n      </div>\n\n        <div id=\"reactjs\">\n        <div class=\"reactjsSkill\">\n          <span> React.JS <i class=\"fab fa-react\"></i> </span>\n          <span> 75% </span>\n        </div>\n        <div class=\"reactjsSkillBar\">\n          <div class=\"reactjsSkillBarLength\"></div>\n        </div>\n      </div>\n\n        <div id=\"tailwind\">\n        <div class=\"tailwindSkill\">\n          <span> Tailwind <img src=\"https://cdn.worldvectorlogo.com/logos/tailwindcss.svg\" alt=\"Tailwind CSS Logo\" style=\"width: 20px; height: 20px; filter: brightness(0) saturate(100%);\">\n          </span>\n          <span> 90% </span>\n        </div>\n        <div class=\"tailwindSkillBar\">\n          <div class=\"tailwindSkillBarLength\"></div>\n        </div>\n      </div>\n     </div>  \n          ";
 }
 //inner HTML for Experience
 if (!expContent) {
     expContent = document.createElement("div");
     expContent.id = "exp-content";
-    expContent.innerHTML = "\n            <h3 class=\"newHead\">My Experience:</h3>\n            <ul>\n            <li>HTML, CSS & Javascript (3Years)</li>\n            <li>Currently Enrolled in GIAIC Program and Learning New Skills.</li>\n            </ul>\n            ";
+    expContent.innerHTML = "\n            <h3 class=\"newHead\">My Experience</h3>\n            <ul>\n            <li>HTML, CSS & Javascript (3Years)</li>\n            <li>Currently Enrolled in GIAIC Program and Learning New Skills.</li>\n            </ul>\n            ";
 }
 //functionality for toggling button
 function toggleButton(content) {
@@ -36,6 +36,12 @@ function toggleButton(content) {
     }
 }
 //applying functionality
-edu === null || edu === void 0 ? void 0 : edu.addEventListener('click', function () { toggleButton(eduContent); });
-skills === null || skills === void 0 ? void 0 : skills.addEventListener('click', function () { toggleButton(skillsContent); });
-exp === null || exp === void 0 ? void 0 : exp.addEventListener('click', function () { toggleButton(expContent); });
+edu === null || edu === void 0 ? void 0 : edu.addEventListener("click", function () {
+    toggleButton(eduContent);
+});
+skills === null || skills === void 0 ? void 0 : skills.addEventListener("click", function () {
+    toggleButton(skillsContent);
+});
+exp === null || exp === void 0 ? void 0 : exp.addEventListener("click", function () {
+    toggleButton(expContent);
+});
--- MileStone-1&2/script.ts
+++ MileStone-1&2/script.ts
@@ -14,11 +14,12 @@ if (!eduContent) {
   eduContent = document.createElement("div");
   eduContent.id = "edu-content";
   eduContent.innerHTML = `
-    <h3 class="newHead">My Education:</h3>
+    <h3 class="newHead">My Education</h3>
     <ul>
     <li><strong>Completed</strong> SSC Part(1&2).</br> 2022-2024</li>
     <li><strong>Studying</strong> at (AGSC)Adamjee Govt Science College Karachi. </br> 2024-Onwards</li>
-    </ul>`;
+    </ul>
+`;
 }
 
 //inner HTML for skills
@@ -26,16 +27,80 @@ if (!skillsContent) {
   skillsContent = document.createElement("div");
   skillsContent.id = "skills-content";
   skillsContent.innerHTML = `
-        <h3 class="newHead">My Skills:</h3>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>Next.JS</li>
-          <li>React.JS</li>
-          <li>Tailwind CSS</li>
-          </ul>
+        <h3 class="newHead">My Skills</h3>
+        
+    <div id= "allSkills">
+        <div id="html">
+        <div class="htmlSkill">
+          <span> HTML <i class="fab fa-html5"></i> </span>
+          <span class="text-md font-bold text-rose-900"> 90% </span>
+        </div>
+        <div class="htmlSkillBar">
+          <div class="htmlSkillBarLength"></div>
+        </div>
+      </div>
+
+        <div id="css">
+        <div class="cssSkill">
+          <span> CSS <i class="fab fa-css3"></i> </span>
+          <span> 75% </span>
+        </div>
+        <div class="cssSkillBar">
+          <div class="cssSkillBarLength"></div>
+        </div>
+      </div>
+
+        <div id="js">
+        <div class="jsSkill">
+          <span> JavaScript <i class="fab fa-js"></i> </span>
+          <span> 80% </span>
+        </div>
+        <div class="jsSkillBar">
+          <div class="jsSkillBarLength"></div>
+        </div>
+      </div>
+
+        <div id="ts">
+        <div class="tsSkill">
+          <span> TypeScript <i class="fab fa-js"></i> </span>
+          <span> 90% </span>
+        </div>
+        <div class="tsSkillBar">
+          <div class="tsSkillBarLength"></div>
+        </div>
+      </div>
+
+        <div id="nextjS">
+        <div class="nextjsSkill">
+          <span> Next.JS <img src="https://cdn.worldvectorlogo.com/logos/next-js.svg" alt="Next.js Logo" style="width: 20px; height: 20px;"> </span>
+          <span> 80% </span>
+        </div>
+        <div class="nextjsSkillBar">
+          <div class="nextjsSkillBarLength"></div>
+        </div>
+      </div>
+
+        <div id="reactjs">
+        <div class="reactjsSkill">
+          <span> React.JS <i class="fab fa-react"></i> </span>
+          <span> 75% </span>
+        </div>
+        <div class="reactjsSkillBar">
+          <div class="reactjsSkillBarLength"></div>
+        </div>
+      </div>
+
+        <div id="tailwind">
+        <div class="tailwindSkill">
+          <span> Tailwind <img src="https://cdn.worldvectorlogo.com/logos/tailwindcss.svg" alt="Tailwind CSS Logo" style="width: 20px; height: 20px; filter: brightness(0) saturate(100%);">
+          </span>
+          <span> 90% </span>
+        </div>
+        <div class="tailwindSkillBar">
+          <div class="tailwindSkillBarLength"></div>
+        </div>
+      </div>
+     </div>  
           `;
 }
 
@@ -44,7 +109,7 @@ if (!expContent) {
   expContent = document.createElement("div");
   expContent.id = "exp-content";
   expContent.innerHTML = `
-            <h3 class="newHead">My Experience:</h3>
+            <h3 class="newHead">My Experience</h3>
             <ul>
             <li>HTML, CSS & Javascript (3Years)</li>
             <li>Currently Enrolled in GIAIC Program and Learning New Skills.</li>
@@ -62,8 +127,13 @@ function toggleButton(content: HTMLElement | null) {
   }
 }
 
-
 //applying functionality
-edu?.addEventListener('click',()=>{ toggleButton(eduContent)})
-skills?.addEventListener('click',()=>{ toggleButton(skillsContent)})
-exp?.addEventListener('click',()=>{ toggleButton(expContent)})
+edu?.addEventListener("click", () => {
+  toggleButton(eduContent);
+});
+skills?.addEventListener("click", () => {
+  toggleButton(skillsContent);
+});
+exp?.addEventListener("click", () => {
+  toggleButton(expContent);
+});
